6de00b3835857c7becd356a3e9b1867afd5887ed
301083645 <67066443+301083645@users.noreply.github.com>
2020-08-18 21:07:36 -0400

    Add project files.

---
+++ COMP123-S2016-FinalExam/SplashForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_FinalExam
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {
+            SplashFormTimer.Enabled = true;
+        }
+
+        private void SplashForm_Load(object sender, EventArgs e)
+        {
+            // move to GenerateNameForm 
+            Program.generateNameForm.Show();
+            // let this form not to be seen
+            this.Hide();
+            // turn of the timer
+            SplashFormTimer.Enabled = false;
+        }
+    }
+}
